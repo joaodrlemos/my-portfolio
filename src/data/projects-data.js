8079ea21979a6bd795402b084bfa9ab94545799b
Lemos <37798301+Lemos467@users.noreply.github.com>
2022-07-26 17:14:58 +0100

    Updated projects section with Hydra Project

--- src/data/projects-data.js
+++ src/data/projects-data.js
@@ -9,18 +9,18 @@ export const projectsData = [
     },
     {
         id: "1",
+        name: "Hydra",
+        descEn: "A proof of Concept app for displaying, searching and filtering users.",
+        descPt: "Uma aplicação prova de conceito para listar, procurar e filtrar utilizadores",
+        img: `url(${require("../assets/pictures/projects/Hydra-preview.png")})`,
+        url: "https://hydrausers.netlify.app",
+    },
+    {
+        id: "2",
         name: "Leminflix",
         descEn: "A Netflix clone app with movie and TV show info.",
         descPt: "Uma aplicação clone da Netflix, com dados de filmes e series.",
         img: `url(${require("../assets/pictures/projects/leminflix.png")})`,
         url: "https://leminflix.netlify.app",
-    },
-    {
-        id: "2",
-        name: "myPortfolio",
-        descEn: "My personal portfolio page. This website",
-        descPt: "O meu portfolio pessoal. Esta página",
-        img: `url(${require("../assets/pictures/projects/portfolio.png")})`,
-        url: "#home",
     },
 ];
