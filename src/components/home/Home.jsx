abf2a6d5cb5863a013469f587586210f76f97455
Lemos <joaodrlemos@gmail.com>
2023-08-24 20:50:44 +0100

    home description fixed

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,36 +1,43 @@
 import "../home/home.scss";
 import { TiArrowDownThick } from "react-icons/ti";
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 
 export default function Home() {
-    const {lang} = useSelector((state) => state);
-    const [onLoad, setOnLoad] = useState(false);
+  const { lang } = useSelector((state) => state);
+  const [onLoad, setOnLoad] = useState(false);
 
-    useEffect(() => {
-        setOnLoad(false);
-        setTimeout(() => setOnLoad(true), 500);
-    }, [lang]);
+  useEffect(() => {
+    setOnLoad(false);
+    setTimeout(() => setOnLoad(true), 500);
+  }, [lang]);
 
-    return (
-        <div className="home" id='home'>
-            <div className="container">
-                <div className="left" />
-                <div className="greeting">
-                    <div className={onLoad ? "wrapper show" : "wrapper hide"}>
-                        <h2>{onLoad && (lang === 'en' ? 'Hi there!' : 'Olá! Eu sou o')}</h2>
-                        <h1>{onLoad && (lang === 'en' && onLoad ? "I'm " : '')}
-                            <span>João Lemos</span>
-                            {onLoad && (lang === 'en' ? "" : ' sou')}
-                        </h1>
-                        <h3><span>Front-end Developer</span></h3>
-                        
-                    </div>
-                </div>
-                <a className="scroll-arrow" href="#about">
-                    <TiArrowDownThick />
-                </a>
-            </div>
+  return (
+    <div className="home" id="home">
+      <div className="container">
+        <div className="left" />
+        <div className="greeting">
+          <div className={onLoad ? "wrapper show" : "wrapper hide"}>
+            <h2>{onLoad && (lang === "en" ? "Hi there!" : "Olá! Eu sou o")}</h2>
+            <h1>
+              {onLoad && (lang === "en" && onLoad ? "I'm " : "")}
+              <span>João Lemos</span>
+              {onLoad && (lang === "en" ? "" : " sou")}
+            </h1>
+            <h3>
+              <span>
+                {onLoad &&
+                  (lang === "pt"
+                    ? "Desenvolvedor Front-end"
+                    : "Front-end Developer")}
+              </span>
+            </h3>
+          </div>
         </div>
-    );
+        <a className="scroll-arrow" href="#about">
+          <TiArrowDownThick />
+        </a>
+      </div>
+    </div>
+  );
 }
